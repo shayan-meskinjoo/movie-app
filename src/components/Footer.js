ffda1603c3b3d990b516fb0c8f3c261fd31c235f
shayan <shayan.meskinjoo@yahoo.com>
2023-06-24 02:24:56 -0700

    new

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import github from "./images/github_PNG37.png";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
-    <div className='footer'>
+    <div className="footer">
       <h4>Movie App all right reserved</h4>
+      <a href="https://github.com/shayan-meskinjoo/movie-app" target="_blank">
+        <img src={github}></img>
+      </a>
     </div>
-  )
+  );
 }
